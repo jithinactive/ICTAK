cfc1c69ae0e504556730d446d47b2664c4051c07
jithinactive <115936158+jithinactive@users.noreply.github.com>
2022-11-11 15:48:30 +0530

    Updated code with dropdown click
    Coded by Specter

--- TrainerManagementSystem/src/test/java/com/tms/signuptest/SignupClass.java
+++ TrainerManagementSystem/src/test/java/com/tms/signuptest/SignupClass.java
@@ -21,13 +21,13 @@ import com.tms.signuppage.SignUpElements;
 public class SignupClass extends TestBase {
 	SignUpElements objSign;
 	
-	@Test
+	@Test(priority=1)
 	public void details() throws IOException {
 		objSign=new SignUpElements(driver);
 		
 		objSign.navigation();
 		
-	String nameVal=ExcelUtilitiesSignup.getCellData(2, 0);
+		String nameVal=ExcelUtilitiesSignup.getCellData(2, 0);
 		objSign.setName(nameVal);
 		String mailVal=ExcelUtilitiesSignup.getCellData(2, 1);
 		objSign.setEmai1(mailVal);
@@ -44,6 +44,15 @@ public class SignupClass extends TestBase {
 		String designationVal=ExcelUtilitiesSignup.getCellData(2, 7);
 		objSign.setDesignation(designationVal);
 		
+		WebElement down1 = driver.findElement(By.cssSelector("#designation"));
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", down1);
+		
+		WebElement var = driver.findElement(By.xpath("/html/body/app-root/app-sign-up/form/div[5]/div[2]/ng-multiselect-dropdown/div/div[1]/span/span[1]"));
+		var.click();
+		WebElement var1 = driver.findElement(By.cssSelector("li.multiselect-item-checkbox:nth-child(1) > div"));
+		var1.click();
+		
+		
 		//Photo upload
 		File strimage = new File(System.getProperty("user.dir")+"/src/main/resources/"+"ProfImage.jpg");
 		String imgLoad = strimage.getAbsolutePath();
@@ -54,7 +63,7 @@ public class SignupClass extends TestBase {
 		objSign.Signdrop("Cyber Security Analyst ");
 		objSign.Signdrop("Robotic Process Automation ");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));*/
-		WebElement down1 = driver.findElement(By.cssSelector("div[class=multiselect-dropdown]"));
+		/*WebElement down1 = driver.findElement(By.cssSelector("div[class=multiselect-dropdown]"));
 				((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", down1);
 
 				try {
@@ -67,7 +76,7 @@ public class SignupClass extends TestBase {
 
 				  WebElement var1 = driver.findElement(By.cssSelector("li.multiselect-item-checkbox:nth-child(1) > div"));
 				  var1.click();
-		
+		*/
 		
 		String paswrdVal=ExcelUtilitiesSignup.getCellData(2, 8);
 		objSign.setPassword(paswrdVal);
@@ -75,6 +84,13 @@ public class SignupClass extends TestBase {
 		objSign.setRePassword(repassVal);
 		
 //		objSign.clickSignup();
+ 
+		
+		// driver.get("https://trainermanagement.herokuapp.com/signup");
+		
+		 
+			
+		  
 		}
 	
 
